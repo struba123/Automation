4fa6b245a96f68a84645f6e7f0acf0ac98bb1018
struba123 <tmss106@gmail.com>
2022-09-01 12:33:00 +0530

    added search_customer file and modfy edit_customer file...

--- TestCases/TC_Edit_Customer_004.py
+++ TestCases/TC_Edit_Customer_004.py
@@ -1,2 +1,9 @@
-// This is a new Test Case
-//This test will check Editing new customer..
+# This is a new Test Case
+#This test will check Editing new customer..
+ # this is code added newly
+
+ #this is code added newly
+
+ #this is code added newly
+
+ #this is code added newly
